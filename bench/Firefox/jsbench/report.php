ba7a241e61e7739eeb868e37fc03bf1ceb071c9d
Timm Allman <tallman@umass.edu>
2015-01-05 12:20:20 -0500

    ignore fid files

--- bench/Firefox/jsbench/report.php
+++ bench/Firefox/jsbench/report.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	$fh = fopen('../../../results/firefox/js/' . $_SESSION['bld'] . '-' . date('mdHis', time()) . '.txt', 'w');
-	fwrite($fh,$_POST['results']);
+	fwrite($fh, $_POST['results']);
 	fclose($fh);
 	
 	$fid = fopen('fid','r');
