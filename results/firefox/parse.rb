c2d248abbd813fa53c9589fc9be5158742d40513
Timm Allman <tallman@umass.edu>
2015-01-30 15:07:48 -0500

    Update parse scripts

--- results/firefox/parse.rb
+++ results/firefox/parse.rb
@@ -3,75 +3,75 @@
 require 'csv'
 
 bld = 0
-zone = 0
 hoard = 0
 jemalloc = 0
+nedmalloc = 0
 
 bldc = 0.0
-zonec = 0.0
 hoardc = 0.0
 jemallocc = 0.0
+nedmallocc = 0.0
 
 Dir.glob('js/*.txt') do |f|
 	file = File.open(f)
 	file.gets
-	num = file.gets('ms').strip!.delete!('ms').to_i
-  
-	if(!f.index('zone').nil?)
-    zone += num
-		zonec += 1
-  elsif(!f.index('hoard').nil?)
-  	hoard += num
+	num = file.gets('ms').strip!.delete!('ms').to_f
+	
+	if(!f.index('nedmalloc').nil?)
+		nedmalloc += num
+		nedmallocc += 1
+	elsif(!f.index('hoard').nil?)
+		hoard += num
 		hoardc += 1
-  elsif(!f.index('jexxmalloc').nil?)
-  	jemalloc += num
+	elsif(!f.index('jemalloc').nil?)
+		jemalloc += num
 		jemallocc += 1
-  else
-    bld += num
+	else
+		bld += num
 		bldc += 1
-  end
+	end
 end
 
-p 'js'
-p "bld #{bld/bldc}"
-p "zone #{zone/zonec}"
-p "hoard #{hoard/hoardc}"
-p "jemalloc #{jemalloc/jemallocc}"
+puts 'js'
+puts "bld #{bld/bldc}"
+puts "hoard #{hoard/hoardc}"
+puts "jemalloc #{jemalloc/jemallocc}"
+puts "nedmalloc #{nedmalloc/nedmallocc}"
 
 bld = {}
-zone = {}
 hoard = {}
 jemalloc = {}
+nedmalloc = {}
 
 Dir.glob('render/*.txt') do |f|
-  CSV.foreach(f, col_sep: ': ') do |r|
-    key = r[0].to_sym
-    val = r[1].to_f
-    if(!f.index('zone').nil?)
-      zone[r[0]].nil? ? zone[key] = val : zone[key] += val
-    elsif(!f.index('hoard').nil?)
-    	hoard[r[0]].nil? ? hoard[key] = val : hoard[key] += val
-    elsif(!f.index('jexxmalloc').nil?)
-    	jemalloc[r[0]].nil? ? jemalloc[key] = val : jemalloc[key] += val
-    else
-      bld[r[0]].nil? ? bld[key] = val : bld[key] += val
-    end
-  end
+	CSV.foreach(f, col_sep: ': ') do |r|
+		key = r[0].to_sym
+		val = r[1].to_f
+		if(!f.index('nedmalloc').nil?)
+			nedmalloc[r[0]].nil? ? nedmalloc[key] = val : nedmalloc[key] += val
+		elsif(!f.index('hoard').nil?)
+			hoard[r[0]].nil? ? hoard[key] = val : hoard[key] += val
+		elsif(!f.index('jemalloc').nil?)
+			jemalloc[r[0]].nil? ? jemalloc[key] = val : jemalloc[key] += val
+		else
+			bld[r[0]].nil? ? bld[key] = val : bld[key] += val
+		end
+	end
 end
 
-p 'render'
+puts 'render'
 total = 0
 bld.each_value {|v| total += v}
-p "bld #{total/bld.size}"
-
-total = 0
-zone.each_value {|v| total += v}
-p "zone #{total/zone.size}"
+puts "bld #{total/bld.size}"
 
 total = 0
 hoard.each_value {|v| total += v}
-p "hoard #{total/hoard.size}"
+puts "hoard #{total/hoard.size}"
 
 total = 0
 jemalloc.each_value {|v| total += v}
-p "jemalloc #{total/jemalloc.size}"
+puts "jemalloc #{total/jemalloc.size}"
+
+total = 0
+nedmalloc.each_value {|v| total += v}
+puts "nedmalloc #{total/nedmalloc.size}"
--- results/sqlite/results.rb
+++ results/sqlite/results.rb
@@ -3,26 +3,18 @@
 require 'csv'
 
 sys = {}
-zone = {}
 mem3 = {}
 mem5 = {}
 hoard = {}
-hoard_zone = {}
 jemalloc = {}
-jemalloc_zone = {}
-tlsf = {}
-tlsf_zone = {}
+nedmalloc = {}
 
 sysc = 0
-zonec = 0
 mem3c = 0
 mem5c = 0
 hoardc = 0
-hoard_zonec = 0
 jemallocc = 0
-jemalloc_zonec = 0
-tlsfc = 0
-tlsf_zonec = 0
+nedmallocc = 0
 
 def parse(file)
 	type = {}
@@ -36,80 +28,46 @@ end
 
 Dir.glob('*.txt') do |f|
 	if(!f.index('sys').nil?)
-    sys = parse(f)
+		sys = parse(f)
 		sysc += 1
 	elsif(!f.index('mem3').nil?)
-    mem3 = parse(f)
+		mem3 = parse(f)
 		mem3c += 1
 	elsif(!f.index('mem5').nil?)
-    mem5 = parse(f)
+		mem5 = parse(f)
 		mem5c += 1
-  elsif(!f.index('hoard').nil?)
-		if(f.index('zone').nil?)
-			hoard = parse(f)
-			hoardc += 1
-		else
-			hoard_zone = parse(f)
-			hoard_zonec += 1
-		end
-  elsif(!f.index('jexxmalloc').nil?)
-		if(f.index('zone').nil?)
-			jemalloc = parse(f)
-			jemallocc += 1
-		else
-			jemalloc_zone = parse(f)
-			jemalloc_zonec += 1
-		end
-  elsif(!f.index('tlsf').nil?)
-		if(f.index('zone').nil?)
-			tlsf = parse(f)
-			tlsfc += 1
-		else
-			tlsf_zone = parse(f)
-			tlsf_zonec += 1
-		end
-  else
-    zone = parse(f)
-		zonec += 1
+	elsif(!f.index('hoard').nil?)
+		hoard = parse(f)
+		hoardc += 1
+	elsif(!f.index('jemalloc').nil?)
+		jemalloc = parse(f)
+		jemallocc += 1
+  elsif(!f.index('nedmalloc').nil?)
+		nedmalloc = parse(f)
+		nedmallocc += 1
   end
 end
 
 total = 0
 sys.each_value {|v| total += v}
-p "sys #{total/sysc}"
-
-total = 0
-zone.each_value {|v| total += v}
-p "zone #{total/zonec}"
+puts "sys		#{total/sysc}"
 
 total = 0
 mem3.each_value {|v| total += v}
-p "mem3 #{total/mem3c}"
+puts "mem3		#{total/mem3c}"
 
 total = 0
 mem5.each_value {|v| total += v}
-p "mem5 #{total/mem5c}"
+puts "mem5		#{total/mem5c}"
 
 total = 0
 hoard.each_value {|v| total += v}
-p "hoard #{total/hoardc}"
-
-total = 0
-hoard_zone.each_value {|v| total += v}
-p "hoard zone #{total/hoard_zonec}"
+puts "hoard		#{total/hoardc}"
 
 total = 0
 jemalloc.each_value {|v| total += v}
-p "jemalloc #{total/jemallocc}"
+puts "jemalloc	#{total/jemallocc}"
 
 total = 0
-jemalloc_zone.each_value {|v| total += v}
-p "jemalloc zone #{total/jemalloc_zonec}"
-
-total = 0
-tlsf.each_value {|v| total += v}
-p "tlsf #{total/tlsfc}"
-
-total = 0
-tlsf_zone.each_value {|v| total += v}
-p "tlsf zone #{total/tlsf_zonec}"
+nedmalloc.each_value {|v| total += v}
+puts "nedmalloc	#{total/nedmallocc}"
