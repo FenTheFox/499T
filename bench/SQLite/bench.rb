bc19228f7b323813e4e1b52b2c7d3727a403c9ea
Timm Allman <tallman@umass.edu>
2015-03-19 23:53:44 -0400

    add new results, something is wrong with perf but commiting what I have

--- bench/SQLite/bench.rb
+++ bench/SQLite/bench.rb
@@ -44,12 +44,12 @@ private
 	def self.run_test(bld, log=nil, lib=nil, perf = -1)
 		cmd = ''
 		cmd += "perf stat -o ../../results/sqlite/perf/#{log}.txt " if perf >= 0
-		cmd += "LD_PRELOAD=../../Replace-Libs/lib#{lib}.so "
+		cmd += "LD_PRELOAD=../../Replace-Libs/lib#{lib}.so " if !lib.nil?
 		cmd += "../../bin/sqlite/sqlite3-#{bld} #{@@itrs} #{@@flags} #{@@schema} #{@@queries} > "
 		if log.nil?
 			cmd += '/dev/null'
 		else
-			cmd += '../../results/sqlite/#{log}.txt'
+			cmd += "../../results/sqlite/#{log}.txt"
 		end
 
 		puts cmd
