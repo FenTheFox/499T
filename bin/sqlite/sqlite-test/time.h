172350dec0aac7a9ce3891c80fee2a7c2dfdc2b6
Timm Allman <tallman@umass.edu>
2015-03-29 21:05:30 -0400

    comment out code not currently being used in time.h

--- bin/sqlite/sqlite-test/time.h
+++ bin/sqlite/sqlite-test/time.h
@@ -16,32 +16,29 @@ class timer {
 	steady_clock::time_point start_time, end_time;
 	long total;
 
-	uint64_t hwstart, hwend, hwtotal;
+	// uint64_t hwstart, hwend, hwtotal;
 
-	uint64_t hwtime() {
-		uint64_t val;
-		__asm__ __volatile__("rdtsc" : "=A"(val));
-		return val;
-	}
+	// uint64_t hwtime() {
+		// uint64_t val;
+		// __asm__ __volatile__("rdtsc" : "=A"(val));
+		// return val;
+	// }
 
 	long diff() {
-		if (start_time == steady_clock::time_point::min() || end_time == steady_clock::time_point::min()) {
+		if (start_time == steady_clock::time_point::min() || end_time == steady_clock::time_point::min())
 			std::cerr << "Please call both start() and end() in between each call to duration()" << std::endl;
-			throw 19;
-		}
+
 		auto ret = duration_cast<nanoseconds>(end_time - start_time);
 
 		return ret.count();
 	}
 
-	uint64_t hwdiff() {
-		if (hwstart == 0 || hwend == 0) {
-			std::cerr << "Please call both start() and end() in between each call to hwduration()" << std::endl;
-			throw 19;
-		}
+	// uint64_t hwdiff() {
+		// if (hwstart == 0 || hwend == 0)
+			// std::cerr << "Please call both start() and end() in between each call to hwduration()" << std::endl;
 
-		return hwend - hwstart;
-	}
+		// return hwend - hwstart;
+	// }
 
   public:
 	timer() {
@@ -54,23 +51,23 @@ class timer {
 
 	void start() {
 		start_time = steady_clock::now();
-		hwstart = hwtime();
+		// hwstart = hwtime();
 	}
 
 	void end() {
 		end_time = steady_clock::now();
-		hwend = hwtime();
+		// hwend = hwtime();
 
 		total += diff();
-		hwtotal += hwdiff();
+		// hwtotal += hwdiff();
 	}
 
 	long total_duration() { return total; }
 
-	uint64_t hwtotal_duration() { return hwtotal; }
+	// uint64_t hwtotal_duration() { return hwtotal; }
 
 	void merge(timer t) {
 		total += t.total_duration();
-		hwtotal += t.hwtotal_duration();
+		// hwtotal += t.hwtotal_duration();
 	}
 };
